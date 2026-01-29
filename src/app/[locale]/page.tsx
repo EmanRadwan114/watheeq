dadd8f2594fceb68fdd961b7f0378238c6e232d5
yousefelsayed18 <yousefabdelmonem18@gmail.com>
2026-01-29 17:31:55 +0200

    Add form validation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,14 +3,16 @@ import LoginCompanies from "@/features/auth/components/LoginCompanies";
 import LoginButton from "@/features/auth/components/LoginContainer/loginButton";
 import LoginIndividualsInputs from "@/features/auth/components/LoginContainer/LoginIndividualsInputs";
 import LoginIndividuals from "@/features/auth/components/LoginIndividuals";
+import LoginValidation from "@/features/auth/FormValidation/LoginValidation";
+import ForgetPassword from "./(auth)/forget-password/page";
+import LoginForm from "@/features/auth/components/LoginForm";
 
 export default function Home() {
   return (
     <>
-      {/* <LoginIndividualsInputs />
-      <LoginButton /> */}
-      <LoginCompanies/>
-      <LoginIndividuals/>
+{/* <LoginValidation/> */}
+<ForgetPassword/>
+{/* <LoginForm/> */}
     </>
   );
 }
