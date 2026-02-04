b3d5149b13c7575290cfb70a7a307a88992217bc
kerolosatef1 <atefkerolos25@gmail.com>
2026-02-04 21:53:42 +0200

    Edit Ar

--- src/features/auth/components/register/OtpContainerRegister.tsx
+++ src/features/auth/components/register/OtpContainerRegister.tsx
@@ -4,23 +4,15 @@ import SingleCodeWithTimer from './OtpTimer';
 const OtpContainerRegister = () => {
 
   const [code, setCode] = useState<number | null>(null);
-
    useEffect(() => {
     setCode(14); 
   }, []);
-
   const handleResend = async () => {
-   
     const newCode = Math.floor(Math.random() * 90) + 10;
     setCode(newCode);
   };
     if (code === null) return null;
-
-
     return <>
-    
-
-    
     <SingleCodeWithTimer
           code={code}
           storageKey="nafath_code_endAt"
